bbbe8febd74b3c527dcdfed33e3a84883cc1d54d
Marcio David <marciodavid81@gmail.com>
2024-07-05 22:08:18 -0300

    feat: add anima-numeros

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -4,11 +4,11 @@ const eventos = ['click', 'touchstart'];
 
 if (menuButton) {
 function openMenu(event) {
-    menuList.classList.toggle('active');
-    menuButton.classList.toggle('active');
+    menuList.classList.add('active');
+    menuButton.classList.add('active');
     outsideClick(menuList, eventos, () => {
         menuList.classList.remove('active');
-        menuButton.classList.remove('active');
+        menuButton.classList.remove('active');  
     })
 }
 
